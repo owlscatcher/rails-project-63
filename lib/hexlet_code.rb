19cdb23ba12b6980ae87d732b1449531ffa4d2aa
Alexander Veselov <collabsis.g@gmail.com>
2024-03-11 00:32:49 +0400

    Included Tag module to main module using autoload method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,7 +2,7 @@
 
 require_relative "hexlet_code/version"
 
-module HexletCode
+module HexletCode # rubocop:disable Style/Documentation
+  autoload :Tag, "./hexlet_code/tag"
   class Error < StandardError; end
-  # Your code goes here...
 end
