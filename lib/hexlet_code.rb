61bd2f435d8088e40f80190b7bff3ae3466ae157
Alexander Veselov <collabsis.g@gmail.com>
2024-03-11 10:16:42 +0400

    Added form_for method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 require_relative "hexlet_code/version"
+require_relative "hexlet_code/tag"
+require_relative "hexlet_code/attributes"
 
 module HexletCode # rubocop:disable Style/Documentation
   autoload :Tag, "./hexlet_code/tag"
+  autoload :Attributes, "./hexlet_code/attributes"
+
   class Error < StandardError; end
+
+  def self.form_for(user, options = {})
+    form_options = if options.key?(:url)
+                     { action: options[:url].to_s, method: "post" }
+                   else
+                     { action: "#", method: "post" }
+                   end
+    Tag.build("form", form_options)
+  end
 end
