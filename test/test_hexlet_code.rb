bc84a964a891ade4c9c446387a4fc45ef5e45ebb
Alexander Veselov <collabsis.g@gmail.com>
2024-03-11 10:15:28 +0400

    Added test

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,7 +3,32 @@
 require "test_helper"
 
 class TestHexletCode < Minitest::Test
+  def setup
+    test_user = Struct.new("User", :name, :job, keyword_init: true)
+    @user = test_user.new name: "rob"
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
+  end
+
+  def test_generate_single_tag
+    expect = "<img src=\"path/to/img\">"
+    assert(::HexletCode::Tag.build("img", { src: "path/to/img" }) == expect)
+  end
+
+  def tast_generate_tag_with_content
+    expect = "<div class=\"card shadow\">Card content</div>"
+    assert(::HexletCode::Tag.build("div", { class: "card shadow" }) { "Card content" } == expect)
+  end
+
+  def test_generate_empty_form
+    expect = "<form action=\"#\" method=\"post\"></form>"
+    assert(::HexletCode.form_for(@user) == expect)
+  end
+
+  def test_generate_empty_form_with_url
+    expect = "<form action=\"path/to/route\" method=\"post\"></form>"
+    assert(::HexletCode.form_for(@user, { url: "path/to/route" }) == expect)
   end
 end
